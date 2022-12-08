6a07213d4b7d70b3190c31077e0dcea150e1337d
mnalamaru4 <s222315268@deakin.edu.au>
2022-12-09 10:35:34 +1100

    week 4

--- server.js
+++ server.js
@@ -1,25 +1,94 @@
 var express = require("express")
 var app = express()
+var cors = require('cors');
+let projectCollection;
+
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
-app.use(express.urlencoded({extends: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
-// const addNumbers = (number1, number2) => {
-//     var num1 = parseInt(number1)
-//     var num2 = parseInt(number2)
-//     var result = num1+num2
-//     return result; 
-// }
+//mongodb connection
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb+srv://Mahidhar:<9989>@cluster0.iu84sbl.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, {useNewUrlParser: true})
 
-// app.get("/addTwoNumbers", (req,res) => {
-//     var number1 = req.query.number1;
-//     var number2 = req.query.number2;
-//     var result = addNumbers(number1, number2)
-//     res. json({statusCode: 200, data: result, message: 'success'})
 
-// })
+
+const createCollection = (collectionName) => {
+        client.connect((err,db) => {
+                projectCollection = client.db().collection(collectionName);
+                if(!err){
+                        console.log('MongoDb Connected')
+                }
+                else{
+                  console.log("DB Error: ", err);
+                  process.exit(1); 
+                }     
+        
+
+        })
+}
+
+// insert project
+const insertProjects = (project,callback) => {
+        projectCollection.insert(project,callback);
+
+}
+
+// post api
+app.post('/api/projects'(req,res) => {
+        console.log("New Project added", req.body)
+        var newProject = req.body
+        insertProjects(newProject,(err,result) => {
+                if(err) {
+                        res.json({status code: 400, message: err})
+                }
+                else {
+                        res.json({statusCode:200, message:"Project Scuccessfully added", data: result})
+                }
+        })
+})
+
+// const cardList = [
+//     {
+//         title: "DANE",
+//         image: "images/dane.jpg",
+//         link: "About DANE",
+//         desciption: "Demo desciption about kitten 2"
+//     },
+//     {
+//         title: "GREAT DANE",
+//         image: "images/GREAT DANE.jpg",
+//         link: "About GREAT DANE",
+//         desciption: "Demo desciption about GREAT DANE"
+//     }
+// ]
+
+// get project
+const getProjects = (callback) => {
+        projectCollection.find({}).toArray(callback);
+}
+
+app.get('/api/projects',(req,res) => {
+        getProjects((err,result) =>{
+                if(err) {
+                        res.json({statusCode: 400, message: err})
+                }
+                else{
+                        res.json({sttausCode:200, message:"Success", data: result})
+                }
+        })
+})
+
+var port = process.env.port || 3000;
+
+app.listen(port,()=>{
+    console.log("App listening to: "+port)
+})
 var port = process.env.port || 3000;
 app.listen(port,()=>{
         console.log("App listening to http://localhost:"+port)
+        createCollection('Pets')
 })
